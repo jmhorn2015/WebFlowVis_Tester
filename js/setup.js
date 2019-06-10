2c09c5baac66957cc45f73d1e5fd992274f2ff71
unknown <jmhorn2015@gmail.com>
2019-06-10 10:42:31 -0500

    Adding local menus

--- js/setup.js
+++ js/setup.js
@@ -101,9 +101,6 @@ var gui;
         INTERSECTED = intersects[0].object;
         INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
         INTERSECTED.material.emissive.setHex(0xff0000);
-		if(currObject != null){
-			currObject.removeMenu();
-		}
 		for(b = 0; b < objects.length; b++){
 			if(INTERSECTED.name = surfaceObjects[b].object.name){
 				currObject = surfaceObjects[b];
@@ -121,7 +118,6 @@ var gui;
       INTERSECTED = null;
 
       currObject.removeMenu();
-	  currObject = null;
 
 
     }
