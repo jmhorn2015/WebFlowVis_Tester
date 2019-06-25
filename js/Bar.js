fdbb5d4d521afb2542c5841ab7f6f23ac0896b8c
unknown <jmhorn2015@gmail.com>
2019-06-24 22:42:45 -0500

    add borders and other graphs

--- js/Bar.js
+++ js/Bar.js
@@ -10,3 +10,5 @@ d3.select(".d3_Bar")
   .enter().append("div")
     .style("width", function(d) { return x(d) + "px"; })
     .text(function(d) { return d; });
+  .attr('width', 920)
+  .attr('height', 250);
