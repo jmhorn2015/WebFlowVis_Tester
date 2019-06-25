be37375ffc498bf0e402d14d21ef50945b4703de
unknown <jmhorn2015@gmail.com>
2019-06-24 22:39:20 -0500

    add borders and other graphs

---
+++ js/Bar.js
@@ -0,0 +1,12 @@
+var dataBar = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".d3_Bar")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
