cbc546b9452b108a8bf86dcdc836556e36844723
unknown <jmhorn2015@gmail.com>
2019-07-01 23:01:15 -0500

    volume rendering

--- js/setup.js
+++ js/setup.js
@@ -30,11 +30,10 @@ var canvas = document.createElement( 'canvas' );
 container.appendChild(canvas);
 var context = canvas.getContext( 'webgl2' );
 var rendererH = new THREE.WebGLRenderer( { canvas: canvas, context: context } );
-rendererH.domElement.id = "rendererH";
 rendererH.setPixelRatio( window.devicePixelRatio );
 rendererH.setSize( w, h);
 container.appendChild( rendererH.domElement );
-$(container).find("rendererH").style.display = "none";
+rendererH.domElement.style.display = "none";
 
 			// Create camera (The volume renderer does not work very well with perspective yet)
 var frusth = 512; // frustum height
