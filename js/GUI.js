a226094481cd34c93efdd16d270b1a66993c1b91
unknown <jmhorn2015@gmail.com>
2019-07-02 07:55:14 -0500

    volume rendering

--- js/GUI.js
+++ js/GUI.js
@@ -25,9 +25,13 @@
 	  if(!sceneCheck){
 		  controls = new THREE.TrackballControls( cameraH, document.getElementById("surface_view"));
 		  controls.enableKeys = false;
+		  rendererH.domElement.style.display = "none";
+		  renderer.domElement.style.display = "blocked";
 	  }
 	  else{
 		controls = new THREE.TrackballControls( camera, document.getElementById("surface_view"));
 		controls.enableKeys = false;
+		rendererH.domElement.style.display = "blocked";
+		renderer.domElement.style.display = "none";
 	  }
   }
