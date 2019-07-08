21ae4eb1eff32adca7b563c268d84ad51b6cf4e6
unknown <jmhorn2015@gmail.com>
2019-07-07 20:30:56 -0500

    2d tester

--- js/AddObject.js
+++ js/AddObject.js
@@ -34,7 +34,7 @@ function AddObject(name2, sceneName){
 
 function AddVolume(name, textureName,  sceneName){
 	console.log("enter");
-	new NRRDLoader.load( name, function ( volume ) {
+	new THREE.NRRDLoader.load( name, function ( volume ) {
 		console.log("start");
 		var texture = new THREE.DataTexture3D( volume.data, volume.xLength, volume.yLength, volume.zLength );
 		texture.format = THREE.RedFormat;
