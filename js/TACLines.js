37481c22bb7c5819ba72f793420fb72e4f308020
unknown <jmhorn2015@gmail.com>
2019-07-19 14:16:21 -0500

    testing

--- js/TACLines.js
+++ js/TACLines.js
@@ -171,13 +171,13 @@ function selectLine(){
 		.attr('stroke', "#888888");
 		return clicked === this;
 	});
-	console.log(clicked.style.origColor);
+	console.log(clicked.origColor);
 	d3.select(clicked).moveToFront()
 	.attr('stroke-width', null)
 	.attr('stroke-width', "6")
 	.attr('stroke', null)
-	.attr('stroke', clicked.style.origColor);
-		console.log(clicked.style.stroke);
+	.attr('stroke', clicked.origColor);
+		console.log(clicked.stroke);
 	for( var b = 0; b < objects.length; b++){
 		if(clicked.id == surfaceObjects[b].object.name){
 			currObject = surfaceObjects[b];
