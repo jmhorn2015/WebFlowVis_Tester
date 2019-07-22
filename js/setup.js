ed6ad8f87145936f7bb2a9b95fe071e4c20b863e
unknown <jmhorn2015@gmail.com>
2019-07-22 00:30:55 -0500

    testing

--- js/setup.js
+++ js/setup.js
@@ -107,70 +107,70 @@ var canvasBounds = renderer.context.canvas.getBoundingClientRect();
     mouse.y = - ((event.clientY - canvasBounds.top) / (canvasBounds.bottom - canvasBounds.top)) * 2 + 1;
 
     raycaster.setFromCamera(mouse, camera);
-	//if(sceneCheck){
-		var intersects = raycaster.intersectObjects(objects, true);
-		if (intersects.length > 0) {
-			if (INTERSECTED != intersects[0].object) {
-				if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-				INTERSECTED = intersects[0].object;
+	var intersects = raycaster.intersectObjects(objects, true);
+	if (intersects.length > 0) {
+		if (INTERSECTED != intersects[0].object) {
+			if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+			INTERSECTED = intersects[0].object;
+			if(!(INTERSECTED instanceof SRVolume)){
 				INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
 				INTERSECTED.material.emissive.setHex(0xff0000);
-				if(currObject != null){
-					currObject.removeMenu();
-					currObject = null;
-				}
-				if(!(INTERSECTED instanceof SRSeedingCurve)){
-					d3.selectAll("path").classed("line", function() {
-						d3.select(this)
-						.attr('stroke', null)
-						.attr('stroke', this.getAttribute('origColor'));
-					});
+			}
+			if(currObject != null){
+				currObject.removeMenu();
+				currObject = null;
+			}
+			if(!(INTERSECTED instanceof SRSeedingCurve)){
+				d3.selectAll("path").classed("line", function() {
+					d3.select(this)
+					.attr('stroke', null)
+					.attr('stroke', this.getAttribute('origColor'));
+				});
+				d3.select(clicked)
+				.attr('stroke-width', null)
+				.attr('stroke-width', "3");
+			}
+			for( var b = 0; b < objects.length; b++){
+				if(INTERSECTED.name == surfaceObjects[b].object.name){
+					currObject = surfaceObjects[b];
+					$('#localGUI').append(currObject.getGUIMenu().domElement);
 					d3.select(clicked)
 					.attr('stroke-width', null)
 					.attr('stroke-width', "3");
+					d3.selectAll("path").classed("line", function() {
+					d3.select(this)
+					.attr('stroke', null)
+					.attr('stroke', "#888888");
+					});
+					clicked = document.getElementById(currObject.object.name);
+					d3.select(clicked).moveToFront()
+					.attr('stroke-width', null)
+					.attr('stroke-width', "6")
+					.attr('stroke', null)
+					.attr('stroke', clicked.getAttribute('origColor'));
+					break;
 				}
-				for( var b = 0; b < objects.length; b++){
-					if(INTERSECTED.name == surfaceObjects[b].object.name){
-						currObject = surfaceObjects[b];
-						$('#localGUI').append(currObject.getGUIMenu().domElement);
-						d3.select(clicked)
-						.attr('stroke-width', null)
-						.attr('stroke-width', "3");
-						d3.selectAll("path").classed("line", function() {
-						d3.select(this)
-						.attr('stroke', null)
-						.attr('stroke', "#888888");
-						});
-						clicked = document.getElementById(currObject.object.name);
-						d3.select(clicked).moveToFront()
-						.attr('stroke-width', null)
-						.attr('stroke-width', "6")
-						.attr('stroke', null)
-						.attr('stroke', clicked.getAttribute('origColor'));
-						break;
-					}
-					if(b+1 == objects.length){
-						console.log("not found");
-					}
+				if(b+1 == objects.length){
+					console.log("not found");
 				}
 			}
 		}
-		else {
-			if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
-			INTERSECTED = null;
+	}
+	else {
+		if (INTERSECTED) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+		INTERSECTED = null;
 
-			currObject.removeMenu();
-			currObject = null;
-			d3.selectAll("path").classed("line", function() {
-				d3.select(this)
-				.attr('stroke', null)
-				.attr('stroke', this.getAttribute('origColor'));
-			});
-			d3.select(clicked)
-			.attr('stroke-width', null)
-			.attr('stroke-width', "3");
-		}
-	//}
+		currObject.removeMenu();
+		currObject = null;
+		d3.selectAll("path").classed("line", function() {
+			d3.select(this)
+			.attr('stroke', null)
+			.attr('stroke', this.getAttribute('origColor'));
+		});
+		d3.select(clicked)
+		.attr('stroke-width', null)
+		.attr('stroke-width', "3");
+	}
 
   }
 
