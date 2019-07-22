deff8fafbbf52888fc4e6a2b600fb174e5e6325b
unknown <jmhorn2015@gmail.com>
2019-07-22 00:45:21 -0500

    testing

--- js/setup.js
+++ js/setup.js
@@ -110,6 +110,7 @@ var canvasBounds = renderer.context.canvas.getBoundingClientRect();
 	var intersects = raycaster.intersectObjects(objects, true);
 	if (intersects.length > 0) {
 		if (INTERSECTED != intersects[0].object) {
+			console.log(INTERSECTED.name);
 			if (INTERSECTED && !(INTERSECTED instanceof SRVolume)) INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
 			INTERSECTED = intersects[0].object;
 			if(!(INTERSECTED instanceof SRVolume)){
