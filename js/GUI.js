42e3172cd97396409f4865dbbdd26aa0aba28714
unknown <jmhorn2015@gmail.com>
2019-07-27 18:27:11 -0500

    testing

--- js/GUI.js
+++ js/GUI.js
@@ -21,7 +21,7 @@ function loadLocal(){
 	}
 	var filename = $(document.getElementById("input"));
 	var filetype = filename.val().split('.').slice(-1)[0];
-	console.log(filename.files);
+	console.log(filename);
 	console.log(filetype);
 	if(filetype == "txt"){
 		console.log("run")
