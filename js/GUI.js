f8aa82a87088477231f162477e78c5a1c31fc7a0
unknown <jmhorn2015@gmail.com>
2019-07-27 12:28:10 -0500

    testing

--- js/GUI.js
+++ js/GUI.js
@@ -10,13 +10,6 @@ var scene;
 var sceneH;
 var mouse = new THREE.Vector2(), INTERSECTED;
 
-//Input reading
-$('.custom-file-input').on('change', function() { 
-	console.log("load");
-   let fileName = $(this).val().split('\\').pop(); 
-   $(this).next('.custom-file-label').addClass("selected").html(fileName); 
-});
-
   // Skybox Cube
   var path = "data/skybox/";
   var urls = [
