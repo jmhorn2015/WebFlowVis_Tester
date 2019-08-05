2f62bb3fc8aca2000393f4e1e5f25d4df7ca7f25
unknown <jmhorn2015@gmail.com>
2019-08-05 17:10:37 -0500

    testing

--- js/Object.js
+++ js/Object.js
@@ -252,7 +252,7 @@ class SRMesh extends SRObject{
 		this.geo = new THREE.PlaneGeometry(9,9,32);
 		this.mat = new THREE.MeshPhongMaterial( { color: 0x888888, dithering: true } );
 		if(shape == "Sphere"){
-			this.geo = new THREE.SphereGeometry(5,32,32);
+			this.geo = new THREE.SphereGeometry(.5,4,4);
 			newName = "Sphere";
 		}
 		else if(shape == "Box"){
