2c06fee4a1f2f9b2b34514fa0a312c0974e19963
unknown <jmhorn2015@gmail.com>
2019-08-09 22:42:52 -0500

    testing

--- js/GUI.js
+++ js/GUI.js
@@ -187,7 +187,7 @@ textureCube.format = THREE.RGBFormat;
   }
   function hiddenObjectList(){
 	  console.log("start");
-	  if(!document.getElementById("dropdownMenuHiddenObj").getAttribute("aria-expanded")){
+	  if(!(document.getElementById("dropdownMenuHiddenObj").getAttribute("aria-expanded"))){
 		console.log("start load");
 		var e = document.getElementById("hiddenObjects");
 		var objPointer;
