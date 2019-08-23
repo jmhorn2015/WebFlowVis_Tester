caaf0e8cb654bf8ec18c6394096cf7dc62df8ff4
unknown <jmhorn2015@gmail.com>
2019-08-22 22:28:19 -0500

    testing

--- js/GUI.js
+++ js/GUI.js
@@ -17,6 +17,49 @@ var mouse = new THREE.Vector2(), INTERSECTED;
 
 var fileStorage;
 
+function loadLocal(evt){
+	fileStorage = evt.target.files;
+	console.log(fileStorage[0]);
+	document.getElementById("inputName").innerHTML = fileStorage[0].name;
+}
+function readLocal(){
+	var currScene;
+	if(sceneCheck){
+		currScene = scene;
+	}
+	else{
+		currScene = sceneH;
+	}
+	var filetype = $(document.getElementById("input")).val().split('.').slice(-1)[0];
+	
+	var reader = new FileReader();
+    reader.onload = (function(theFile) {
+        return function(e) {
+			if(filetype == "txt"){
+				GenerateCurves(e.target.result, currScene, true);
+			}
+			else if(filetype == "obj"){
+				AddObject(e.target.result, currScene, true);
+			}
+			else if(filetype == "nrrd"){
+				
+			}
+			else if(filetype == "vtk"){
+				AddVTKVolume(e.target.result, currScene, true);
+			}
+			else{
+				alert("Error: Bad File Type");
+			};	
+        };
+      })(fileStorage[0]);
+	  if(filetype == "txt"){
+		reader.readAsText(fileStorage[0]);
+	  }
+	  else{
+		reader.readAsDataURL(fileStorage[0]);
+	  }
+}
+
   // Skybox Cube
   var path = "data/skybox/";
   var urls = [
