c4bba395cfad0c58f28474a14575068a439100f5
unknown <jmhorn2015@gmail.com>
2019-08-24 11:28:32 -0500

    Update Directory

--- js/setup.js
+++ js/setup.js
@@ -4,8 +4,8 @@ import {loadLocal} from "./LocalImport.js";
 import {readLocal} from "./LocalImport.js";
 
 //Load file setup
-$("#input").attr("onchange", loadLocal(event));
-$("#inputGroupFi").attr("onclick", readLocal());
+$("#input").setAttribute("onchange", (loadLocal(event));
+$("#inputGroupFi").setAttribute("onclick", readLocal());
 
 //-----Three.js Setup-----//
 var container = document.createElement( 'div' );
