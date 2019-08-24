a36fd3ef440a2f3607883f9761b2123ca29f0226
unknown <jmhorn2015@gmail.com>
2019-08-24 10:58:35 -0500

    Update Directory

--- js/LocalImport.js
+++ js/LocalImport.js
@@ -11,36 +11,38 @@ export function loadLocal(evt){
 	}
 }
 export function readLocal(){
-	var currScene;
-	if(sceneCheck){
-		currScene = scene;
-	}
-	else{
-		currScene = sceneH;
-	}
-	var filetype = $(document.getElementById("input")).val().split('.').slice(-1)[0];
+	if(fileStorage != null){
+		var currScene;
+		if(sceneCheck){
+			currScene = scene;
+		}
+		else{
+			currScene = sceneH;
+		}
+		var filetype = $(document.getElementById("input")).val().split('.').slice(-1)[0];
 	
-	var reader = new FileReader();
-    reader.onload = (function(theFile) {
-        return function(e) {
-			if(filetype == "txt"){
-				GenerateCurves(e.target.result, currScene, true);
-			}
-			else if(filetype == "obj"){
-				AddObject(e.target.result, currScene, true);
-			}
-			else if(filetype == "vtk"){
-				AddVTKVolume(e.target.result, currScene, true);
-			}
-			else{
-				alert("Error: Bad File Type");
-			};	
-        };
-      })(fileStorage[0]);
-	  if(filetype == "txt"){
-		reader.readAsText(fileStorage[0]);
-	  }
-	  else{
-		reader.readAsDataURL(fileStorage[0]);
-	  }
+		var reader = new FileReader();
+		reader.onload = (function(theFile) {
+			return function(e) {
+				if(filetype == "txt"){
+					GenerateCurves(e.target.result, currScene, true);
+				}
+				else if(filetype == "obj"){
+					AddObject(e.target.result, currScene, true);
+				}
+				else if(filetype == "vtk"){
+					AddVTKVolume(e.target.result, currScene, true);
+				}
+				else{
+					alert("Error: Bad File Type");
+				};	
+			};
+		})(fileStorage[0]);
+		if(filetype == "txt"){
+			reader.readAsText(fileStorage[0]);
+		}
+		else{
+			reader.readAsDataURL(fileStorage[0]);
+		}
+	}
 }
